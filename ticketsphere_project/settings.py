3a21faf2d347a9819c29ead1679d59fc5c2e277b
Moment49 <ibenachoelvis49@gmail.com>
2025-01-27 00:42:03 +0100

    worked on the landing page

--- ticketsphere_project/settings.py
+++ ticketsphere_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'events.apps.EventsConfig',
+    'tickets.apps.TicketsConfig',
+   
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
